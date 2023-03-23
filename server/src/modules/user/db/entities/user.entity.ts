ec281cf2eb631d0e3443a3217282964402d90756
Pedro Gabriel <devpedrogabriel@gmail.com>
2023-03-22 22:13:36 -0300

    feat: UserEntity and Repository

--- server/src/modules/user/db/entities/user.entity.ts
+++ server/src/modules/user/db/entities/user.entity.ts
@@ -1,22 +1,15 @@
-// import { Entity, Column, PrimaryColumn, CreateDateColumn } from 'typeorm';
 import { IUser } from '../../domain/user.domain';
 
-// @Entity()
 export class UserEntity implements IUser {
-  // @PrimaryColumn()
   id: string;
-
-  // @Column()
   name: string;
-
-  // @Column()
   email: string;
-
-  // @Column()
   password: string;
-
-  // @CreateDateColumn({
-  //   type: 'datetime',
-  // })
   created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    this.created_at = this.created_at ?? new Date();
+    this.updated_at = this.updated_at ?? new Date();
+  }
 }
--- server/src/modules/user/db/repositories/user.repository.ts
+++ server/src/modules/user/db/repositories/user.repository.ts
@@ -1,8 +1,38 @@
+import { Injectable } from '@nestjs/common/decorators';
+import { IRepository } from 'src/modules/shared/repositories/repository';
 import { IUser } from '../../domain/user.domain';
+import { UserEntity } from '../entities/user.entity';
+import { Knex } from 'knex';
+import { InjectConnection } from 'nest-knexjs';
 
-export interface IUserRepository {
-  get(id: string): Promise<IUser>;
-  delete(id: string): Promise<void>;
-  save(user: IUser): Promise<void>;
-  update(user: IUser): Promise<IUser>;
+@Injectable()
+export class UserRepository implements IRepository<IUser> {
+  constructor(@InjectConnection() private readonly knex: Knex) {}
+
+  async get(id: string): Promise<IUser> {
+    const user = await this.knex<UserEntity>('users').where('id', id).first();
+    return user;
+  }
+
+  async getByEmail(email: string): Promise<IUser | undefined> {
+    const user = await this.knex<UserEntity>('users')
+      .where('email', email)
+      .first();
+    return user;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.knex('users').where('id', id).del();
+    return;
+  }
+
+  async save(user: IUser): Promise<void> {
+    await this.knex('users').insert(user);
+    return;
+  }
+
+  async update(user: IUser): Promise<IUser> {
+    await this.knex('users').where('id', user.id).update(user);
+    return user;
+  }
 }
