e221d5edce7fa12c8ca35e3f30ccbeaaf1af32fd
Pedro Gabriel <devpedrogabriel@gmail.com>
2023-03-22 22:16:06 -0300

    feat: adding service logics to implement domains

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CryptoService } from '../../shared/services/crypto.service';
 import { UserEntity } from '../db/entities/user.entity';
+import { UserRepository } from '../db/repositories/user.repository';
 import { IUser } from '../domain/user.domain';
 import { IUserService } from '../domain/user.service';
 
 @Injectable()
 export class UserService implements IUserService {
-  constructor(private readonly cryptoService: CryptoService) {}
+  constructor(
+    private readonly cryptoService: CryptoService,
+    private readonly repository: UserRepository,
+  ) {}
+
+  async findUserByEmail(email: string): Promise<IUser> {
+    const entity = await this.repository.getByEmail(email);
+    if (!entity) {
+      throw new Error('Email does not exists');
+    }
+
+    return entity;
+  }
 
   async createUser(user: Omit<IUser, 'id'>): Promise<IUser> {
+    if (await this.repository.getByEmail(user.email)) {
+      throw new Error('Email already exists');
+    }
+
     const entity = new UserEntity();
 
     entity.id = this.cryptoService.generateUUID();
@@ -16,6 +33,22 @@ export class UserService implements IUserService {
     entity.email = user.email;
     entity.password = await this.cryptoService.hashify(user.password);
 
+    this.repository.save(entity);
+
+    return entity;
+  }
+
+  async updateUser(
+    oldEmail: string,
+    user: Pick<IUser, 'email' | 'name'>,
+  ): Promise<IUser> {
+    const entity = await this.repository.getByEmail(oldEmail);
+    if (!entity) {
+      throw new Error('Email does not exists');
+    }
+    entity.name = user.name;
+    entity.email = user.email ?? entity.email;
+    this.repository.update(entity);
     return entity;
   }
 }
